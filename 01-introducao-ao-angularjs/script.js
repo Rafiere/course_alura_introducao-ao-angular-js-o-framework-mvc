a142a63d8510d40d867aff0f77d533e974b206a7
Rafiere <rafapgbr@gmail.com>
2022-12-11 14:12:14 -0300

    feat: adding code for lesson 01

---
+++ 01-introducao-ao-angularjs/script.js
@@ -0,0 +1,13 @@
+/* O AngularJS é um framework MVC para o front-end que foi liberado para o público em 2009. */
+
+/* Ele possui o paradigma MVC. */
+
+/* Ele possui a programação declarativa através de templates. Os templates consomem dados e se redesenham com base nesses dados. */
+
+/* Ele possui o "two-way databinding", uma alteração no model reflete na view, e vice-versa. */
+
+/* Ele possui componentes que podem ser reutilizáveis. */
+
+/* O foco do AngularJS são aplicações SPA. Nesse tipo de aplicação, apenas a página principal é carregada, e, através de um sistema de rotas do framework, o miolo do site é alterado com base na navegação do usuário. */
+
+/* A versão do AngularJS que será utilizada nesse treinamento é a "Angular-1.2.6.min" */
